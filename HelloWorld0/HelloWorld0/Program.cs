1100aec813f68e7c6a557aa0d6d4b6fa24508f97
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-08-25 12:39:53 +0200

    Initial commit

---
+++ HelloWorld0/HelloWorld0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
