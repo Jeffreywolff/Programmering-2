775d426818397b9f2a04949550d7441210fef7b0
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-08-27 11:28:03 +0200

    Added Math

--- HelloWorld0/HelloWorld0/Program.cs
+++ HelloWorld0/HelloWorld0/Program.cs
@@ -6,10 +6,38 @@ namespace HelloWorld0
     {
         static void Main(string[] args)
         {
+            bool alive;
             var age = Console.ReadLine();
             var name = Console.ReadLine();
+            Console.WriteLine("Are you alive? Enter Y or N: ");
+            if(Console.ReadLine() == "Y")
+            {
+                alive = true;
+            }
+            else
+            {
+                alive = false;
+            }
+            if(alive == true)
+            {
+                Console.WriteLine("You're alive");
+            }
+            else
+            {
+                Console.WriteLine("Dead?");
+            }
             Console.WriteLine("Your age is " + age);
             Console.WriteLine("And your name is " + name + "!");
+            Console.WriteLine("Enter a number:");
+            var opt1 = Console.ReadLine();
+            Console.WriteLine("Enter the second number:");
+            var opt2 = Console.ReadLine();
+
+            //Convert to integer
+            var num1 = Convert.ToInt32(opt1);
+            var num2 = Convert.ToInt32(opt2);
+
+            Console.WriteLine("This is the bigger number: " + Math.Max(num1, num2));
         }
     }
 }
