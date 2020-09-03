e795317e7ca2b4e480dbc55a35ae5a05d010a719
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-09-03 13:51:56 +0200

    Commiting the extra assignment

--- HelloWorld0/HelloWorld0/Program.cs
+++ HelloWorld0/HelloWorld0/Program.cs
@@ -1,71 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloWorld0
 {
+
+    class Person
+    {
+        public string Age { get; set;}
+
+        public string Name { get; set;}
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            bool alive;
-            String name;
-            Console.WriteLine("What is your legal age?");
-            var age = Console.ReadLine();
+            var personList = new List<string>();
+            var nameArr = new string[5];
             
 
-            Console.WriteLine("What is your legal name?");
-            name = Console.ReadLine();
-
-            while (true)
-            {   
-                Console.WriteLine("Are you alive? Enter Y or N: ");
-                var answer = Console.ReadLine();
-                if (answer == "Y")
-                {
-                    alive = true;
-                    break;
-                }
-                else if(answer == "N")
-                {
-                    alive = false;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input!");
-                    continue;
-                }
-            }
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("Enter age:  ");
+                var person = new Person();
+                person.Age = Console.ReadLine();
+                personList.Add(person.Age);
+                Console.WriteLine("Enter name:  ");
+                person.Name = Console.ReadLine();
+                personList.Add(person.Name);
             }
-
-            Console.WriteLine("What is better? \n monday or tuesday:");
-            var favDay = Console.ReadLine();
-            switch (favDay)
+            foreach (var item in personList)
             {
-                case "monday":
-                    Console.WriteLine("For real? okey boomer!");
-                    break;
-                case "tuesday":
-                    Console.WriteLine("Me too!");
-                    break;
-                default:
-                    Console.WriteLine("Sure, but that was not what I asked!");
-                    break;
+                Console.WriteLine(item);
             }
-            Console.WriteLine("Your age is " + age);
-            Console.WriteLine("And your name is " + name + "!");
-            Console.WriteLine("Enter a number:");
-            var opt1 = Console.ReadLine();
-            Console.WriteLine("Enter the second number:");
-            var opt2 = Console.ReadLine();
 
-            //Convert to integer
-            var num1 = Convert.ToInt32(opt1);
-            var num2 = Convert.ToInt32(opt2);
 
-            Console.WriteLine("This is the bigger number: " + Math.Max(num1, num2));
+            for (int i = 0; i < nameArr.Length; i++)
+            {
+                Console.WriteLine("Enter name " + (i+1) + ":");
+                var nameItem = Console.ReadLine();
+                nameArr[i] = nameItem;
+            }
+            for (int i = nameArr.Length -1; i > -1; i--)
+            {
+                Console.WriteLine(nameArr[i]);
+            }
+
         }
     }
 }  
